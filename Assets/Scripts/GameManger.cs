a9dbd7eea9c8cd6273c64f8e80b381f780425eee
Unknown <matt.p.holmes@gmail.com>
2018-10-24 20:25:26 +0100

    Test app
    Made inntional app that cahnges between scenes on a button press

---
+++ Assets/Scripts/GameManger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManger : MonoBehaviour {
+
+    private static bool GameManagerExists;
+    private void Start()
+    {
+        if (!GameManagerExists)
+        {
+            GameManagerExists = true;
+            DontDestroyOnLoad(this.gameObject); 
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    public void changeScene(string sceneToChangeTo)
+    {
+        SceneManager.LoadScene(sceneToChangeTo);
+    }
+}
