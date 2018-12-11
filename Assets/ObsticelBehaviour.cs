0cd327a9413e67cbcd030ec14fb012e25493a640
mp2-holmes <matt.p.holmes@gmail.com>
2018-12-11 17:46:28 +0000

    added beat detction and songs and object instantateion

---
+++ Assets/ObsticelBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObsticelBehaviour : MonoBehaviour {
+
+    public float speed = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Move the object forward along its z axis 1 unit/second.
+        transform.Translate(Vector3.back * (speed * Time.deltaTime));
+    }
+}
