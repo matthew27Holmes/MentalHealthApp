79447cb5617380c0e0ee156ee5647f9c2934317b
mp2-holmes <matt.p.holmes@gmail.com>
2019-01-20 16:07:17 +0000

    added movement to new scene needs work added camera follow

---
+++ Assets/BoarderPushBack.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoarderPushBack : MonoBehaviour {
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+
+        }
+    }
+    void pushBackBreeze(GameObject player)
+    {
+
+    }
+    Vector3 findPlayersDirection(Transform player)
+    {
+
+    }
+}
--- Assets/FlyingControls.cs
+++ Assets/FlyingControls.cs
@@ -7,24 +7,32 @@ using UnityEngine.SceneManagement;
 
 public class FlyingControls : MonoBehaviour
 {
-    Vector3 targetPos;
-    public GameObject swipeInput;
-    public Camera mainCamera;
+    #region mouse Control variables
+        Vector2 firstPressPos;
+        Vector2 secondPressPos;
+        Vector2 currentSwipe;
+    #endregion
 
+    #region SwipeVariables
 
-    Vector3 fp;   //First touch position
-    Vector3 lp;   //Last touch position
-    float dragDistance;  //minimum distance for a swipe to be registered
+        Vector3 fp;   //First touch position
+        Vector3 lp;   //Last touch position
+        float dragDistance;  //minimum distance for a swipe to be registered
 
-    //mouse debug control 
-    Vector2 firstPressPos;
-    Vector2 secondPressPos;
-    Vector2 currentSwipe;
+    float touchTimeStart, touchTimeFinish, timeInterval; // to calculate swipe time to sontrol throw force in Z direction
 
+        [SerializeField]
+        float throwForceInXandY = 1f; // to control throw force in X and Y directions
 
+        [SerializeField]
+        float throwForceInZ = 50f; // to control throw force in Z direction
+
+        Rigidbody rb;
+    #endregion
 
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
         dragDistance = Screen.height * 5 / 100; //dragDistance is 15% height of the screen
     }
 
@@ -43,11 +51,12 @@ public class FlyingControls : MonoBehaviour
     {
         if (Input.touchCount == 1)
         {
-
             Touch touch = Input.GetTouch(0);
 
             if (touch.phase == TouchPhase.Began)
             {
+                touchTimeStart = Time.time;
+
                 fp = touch.position;
                 lp = touch.position;
             }
@@ -62,22 +71,19 @@ public class FlyingControls : MonoBehaviour
                 //Check if drag distance is greater than 20% if less then = tap
                 if (Mathf.Abs(lp.x - fp.x) > dragDistance || Mathf.Abs(lp.y - fp.y) > dragDistance)
                 {
-                    if ((lp.x > fp.x))
-                    {
-                        Debug.Log("Right Swipe");
-                    }
-                    else
-                    {
-                        Debug.Log("Left Swipe");
-                    }
-                    if (lp.y > fp.y)
-                    {
-                        Debug.Log("Up Swipe");
+                    // marking time when you release it
+                    touchTimeFinish = Time.time;
 
-                    }
+                    // calculate swipe time interval 
+                    timeInterval = touchTimeFinish - touchTimeStart;
+
+                    // calculating swipe direction in 2D space
+                    Vector2 direction = fp - lp;
+
+                    // add force to balls rigidbody in 3D space depending on swipe time, direction and throw forces
+                    rb.AddForce(-direction.x * throwForceInXandY, -direction.y * throwForceInXandY, throwForceInZ / timeInterval);
                 }
             }
-
         }
     }
 
@@ -85,32 +91,30 @@ public class FlyingControls : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            //save began touch 2d point
-            firstPressPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
-        }
+            touchTimeStart = Time.time;
 
+            firstPressPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);            
+        }
         if (Input.GetMouseButtonUp(0))
         {
+
             //save ended touch 2d point
             secondPressPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
 
             //create vector from the two points
-            currentSwipe = new Vector2(secondPressPos.x - firstPressPos.x, secondPressPos.y - firstPressPos.y);
+            currentSwipe = secondPressPos - firstPressPos;
 
-            //normalize the 2d vector
-            currentSwipe.Normalize();
+            //currentSwipe.Normalize();
 
-            if (currentSwipe.x < 0)
-            {
-            }
-            else if (currentSwipe.x > 0)
-            {
-            }
+            // marking time when you release it
+            touchTimeFinish = Time.time;
 
-            if (currentSwipe.y > 0)
-            {
+            // calculate swipe time interval 
+            timeInterval = touchTimeFinish - touchTimeStart;
 
-            }
+            // add force to balls rigidbody in 3D space depending on swipe time, direction and throw forces
+            rb.AddForce( currentSwipe.x * throwForceInXandY, currentSwipe.y * throwForceInXandY, throwForceInZ / timeInterval);
+
         }
     }
     
