4ef0650966d554017fe7770539b07bf57ab1f684
matt <matt.p.holmes@gmail.com>
2019-04-10 19:26:09 +0100

    scene change

---
+++ Assets/sceneChange.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class sceneChange : MonoBehaviour {
+
+    public void changeScene(string sceneToChangeTo)
+    {
+        // currentScene = sceneToChangeTo;
+        SceneManager.LoadScene(sceneToChangeTo);
+    }
+
+}
